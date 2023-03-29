d80e184cadf00fd994eb74a9ea931ec2eb3600ea
jeremyoduromanu <jeremymanu2206@gmail.com>
2023-03-29 00:31:17 +0000

    fixing js bug

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 // thanks page rating passage code 
 let activeRate = document.getElementsByClassName('rate');
 
-let otherActive = document.getElementsByClassName('active');
 
-for ( let index = 0; index < activeRate.length; index++)
-{
-    activeRate [index].addEventListener('click', addClassName, removeClassName)
 
-    function addClassName (){
+for (let index = 0; index < activeRate.length; index++){
+    activeRate[index].addEventListener('click', addClassName)
+    activeRate[index].addEventListener('click', addSelectedStat)
+
+    function addClassName () {
+
+        let activeLength = document.getElementsByClassName('active');
+    
+        if (activeLength.length >= 1){
+        alert('you have to select only one.');
+        activeLength[index].classList.remove('active');
+
         
-        activeRate [index].classList.add('.active')
-
+    }
+    return activeRate[index].classList.add('active');
     }
 
-    function removeClassName (){
-        otherActive.removeClassName('active')
+    function addSelectedStat(){
+        let selected = document.getElementsByClassName('active')[0];
+
+        let span = document.getElementById('span');
+
+        span.innerText = "You selected " + selected.innerText + " out of 5";
     }
+
+
+    
+
+    
 }
 
-console.log(otherActive)
+
+
+
 
 
 
